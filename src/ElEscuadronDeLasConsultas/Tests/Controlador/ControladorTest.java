d2da9d79e6b5f01c67017aed792a07fc2f2c51c0
Dazdwc <118013018+Dazdwc@users.noreply.github.com>
2023-04-04 19:32:56 +0200

    Versión 1.1 pruebas test4

--- src/ElEscuadronDeLasConsultas/Tests/Controlador/ControladorTest.java
+++ src/ElEscuadronDeLasConsultas/Tests/Controlador/ControladorTest.java
@@ -1,7 +1,48 @@
 package ElEscuadronDeLasConsultas.Tests.Controlador;
 
 
+import ElEscuadronDeLasConsultas.Modelo.*;
+import org.junit.jupiter.api.Test;
+
+import java.text.DecimalFormat;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class ControladorTest {
+        @Test
+        void TestaddClientePremium() {
+
+         ClientePremium cliente = new ClientePremium("mailsito", "pinipon", "12345678A", "123 Callepiruleta");
+         String expected ="\nNombre:             pinipon" +
+             "\nMail:               mailsito" +
+             "\nNif:                12345678A" +
+             "\nDomicilio:          123 Callepiruleta" +
+             "\nTipo de Cliente:    Premium" +
+             "\nCuota anual:        30" +"€" +
+             "\nDescuento en envío: 20" +  "%" +
+             "\n" +
+                 "\nPor ser cliente premiun obtienes las siguientes ventajas:" +
+                 "\nDescuento en envios: 20.0" + "%" +
+                 "\nSu cuota anual es de:30.0" + "€";;
+         assertEquals(expected, cliente.toString());
+        }
+         @Test
+            void TestaddClienteStandar() {
 
 
+        ClienteStandar cliente = new ClienteStandar("mailsito", "pinipon", "12345678A", "123 Callepiruleta");
+        String expected ="\nNombre:             pinipon" +
+                "\nMail:               mailsito" +
+                "\nNif:                12345678A" +
+                "\nDomicilio:          123 Callepiruleta" +
+                "\nTipo de Cliente:    Standar" +
+                "\nCuota anual:        0" +"€" +
+                "\nDescuento en envío: 0" +  "%" +
+                "\n";
+        assertEquals(expected, cliente.toString());
+
+
+    }
 }
--- src/ElEscuadronDeLasConsultas/Tests/Modelo/ListaTest.java
+++ src/ElEscuadronDeLasConsultas/Tests/Modelo/ListaTest.java
@@ -22,42 +22,42 @@ class ListaTest {
 
     @Test
     void aadElemento() {
-        lista.add("Element 1");
+        lista.add("añadimos");
         assertEquals(1, lista.getSize());
         assertFalse(lista.isEmpty());
     }
 
     @Test
     void addMasElemento() {
-        lista.add("Element 1");
-        lista.add("Element 2");
-        lista.add("Element 3");
+        lista.add("añado 1");
+        lista.add("que sean 2");
+        lista.add("venga ponme 3");
         assertEquals(3, lista.getSize());
         assertFalse(lista.isEmpty());
     }
 
     @Test
     void BorradoElementos() {
-        lista.add("Element 1");
-        lista.add("Element 2");
-        lista.borrar("Element 2");
+        lista.add("Esto se queda");
+        lista.add("Esto se va");
+        lista.borrar("Esto se va");
         assertEquals(1, lista.getSize());
         assertFalse(lista.isEmpty());
-        assertEquals("Element 1", lista.getAt(0));
+        assertEquals("Esto se queda", lista.getAt(0));
     }
 
     @Test
     void RecorridogetAt() {
-        lista.add("Element 1");
-        lista.add("Element 2");
-        assertEquals("Element 1", lista.getAt(0));
-        assertEquals("Element 2", lista.getAt(1));
+        lista.add("Primer elemento");
+        lista.add("Segundo elemento");
+        assertEquals("Primer elemento", lista.getAt(0));
+        assertEquals("Segundo elemento", lista.getAt(1));
     }
 
     @Test
     void Limplieza() {
-        lista.add("Element 1");
-        lista.add("Element 2");
+        lista.add("Esto se va");
+        lista.add("como su amor por ti");
         lista.clear();
         assertEquals(0, lista.getSize());
         assertTrue(lista.isEmpty());
@@ -65,18 +65,18 @@ class ListaTest {
 
     @Test
     void PruebaArrays() {
-        lista.add("Element 1");
-        lista.add("Element 2");
+        lista.add("testeando array");
+        lista.add("segundo elemento");
         ArrayList<String> arrList = lista.getArrayList();
         assertEquals(2, arrList.size());
-        assertEquals("Element 1", arrList.get(0));
-        assertEquals("Element 2", arrList.get(1));
+        assertEquals("testeando array", arrList.get(0));
+        assertEquals("segundo elemento", arrList.get(1));
     }
 
     @Test
     void ProbarVacio() {
         assertTrue(lista.isEmpty());
-        lista.add("Element 1");
+        lista.add("esto ya no esta vacío");
         assertFalse(lista.isEmpty());
     }
 
